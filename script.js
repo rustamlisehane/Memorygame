f20fe58fb2f5f1f5c35db9a3e3e50c82cfa9ae46
rustamlisehane <rustemlisehane14@gmail.com>
2024-07-10 17:25:09 +0400

    Start button functionality

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
-// evvelki card ID
-let persistedPairData = '';
-// tapilmis cut kartlarin sayi
-let phoundCount = 0
-// butun kartlarini get elemetbyclassname ile secirik
-var cards = document.getElementsByClassName("card");
-console.log(cards);
-//
-for (var i = 0; i < cards.length; i++) {
-    // her bir kard ucun OnClick function elave edilib
-    cards[i].addEventListener("click", onClick)
-}
-
-function onClick(e) {
-    //klik olunan elementi goturur
-    let targetElemt = e.currentTarget
-    console.log(targetElemt)
-    // bu kartin tapilib tapilmadagini yoxlanir
-    let isPhound = targetElemt.getAttribute('data-isphound') === 'tapildi';
-    // klik olunan kartin Id-sini goturur
-    let pairId = targetElemt.getAttribute('data-pairid');
-
-    console.log('data-pairid', pairId)
-    console.log('data-isphound', isPhound)
-    console.log('data-persistedPairData', persistedPairData)
-    // "tapildi" statusu varsa koddan cixiriq 
-    // yeni istifadeci ikinci defe aciq olan karta klikledikde hecne bas vermir
-    if (isPhound) {
-        return;
+let startButton = document.getElementsByClassName("start-button");
+console.log(startButton)
+startButton[0].addEventListener("click", function () {
+    // əvvəlki yəni birinci acılan kartın idsini götürürük.
+    let persistedPairData = '';
+    // tapılmış cüt kartların sayı .
+    let phoundCount = 0
+    // butun kartlarini get elemetbyclassname ile secirik
+    var cards = document.getElementsByClassName("card");
+    console.log(cards);
+    //kartların içərisindən keçmək üçün loopdan istifadə edirik.
+    for (var i = 0; i < cards.length; i++) {
+        // hər bir kart üçün OnClick function əlavə edilir.
+        cards[i].addEventListener("click", onClick)
     }
 
-    // Karti cevirmek ucun card-open statusu elave edir
-    targetElemt.classList.add('card-open')
+    function onClick(e) {
+        //kilik olunan element yəni(kartı)götürür.
+        let targetElemt = e.currentTarget
+        console.log(targetElemt)
+        //bu kartın tapılıb tapılmadığı yoxlanılır.
+        let isPhound = targetElemt.getAttribute('data-isphound') === 'tapildi';
+        // kilik olunan kartın idsini götürür.
+        let pairId = targetElemt.getAttribute('data-pairid');
 
-    //acilan kartin id goturub nobeti acilan kart ile yoxlayir.
-    if (persistedPairData === '') {
-        persistedPairData = pairId;
-    } else {
-        if (persistedPairData === pairId) {
+        console.log('data-pairid', pairId)
+        console.log('data-isphound', isPhound)
+        console.log('data-persistedPairData', persistedPairData)
+        // "tapıldı" statusu varsa koddan cixiriq 
+        //  istifadəci ikinci dəfə acıq olan karta kliklədikdə həcnə baş vermir.
+        if (isPhound) {
+            return;
+        }
 
-            // eger idler beraberdise onlara tapildi statusu elave edilir 
-            const allPairs = document.querySelectorAll(`[data-pairid='${pairId}']`)
-            allPairs.forEach(function (item) {
-                item.setAttribute('data-isphound', 'tapildi')
-            })
-            persistedPairData = ''
-            //tapilmis kartlarin sayini artirir 
-            phoundCount = phoundCount + 1
-            if (phoundCount === cards.length / 2) {//eger butunn kartlar t6apilibsa You Win! yazisini ekranda gorunur
-                document.getElementById('you-win').style.display = 'block'
-            }
-        } else {//eger data idleri eynidirse onlara "data-pairid" statusu verilir
-            let prevCards = document.querySelectorAll(`[data-pairid='${persistedPairData}']`)
-            prevCards.forEach(function (item) {
-                //hər bir acılan karta "card-open" statusu verilir ancaq o kartlardan sonra kilik etdiyimiz kart ilə idləri bir deilsə həmin kartda "card-open" statusu yox edilir 
-                item.classList.remove('card-open');
-            })
+        // Kartı çevirən zaman card-open statusu əlavə edir.
+        targetElemt.classList.add('card-open')
+
+        //acılan kartın id götürüb nöbəti acılan kart ilə yoxlayır.
+        if (persistedPairData === '') {
             persistedPairData = pairId;
+        } else {
+            if (persistedPairData === pairId) {
+
+                // əgər idlər bəranbərdisə onlara tapıldı statusu bərabər edilir .
+                let allPairs = document.querySelectorAll(`[data-pairid='${pairId}']`)
+                allPairs.forEach(function (item) {
+                    item.setAttribute('data-isphound', 'tapildi')
+                })
+                persistedPairData = ''
+                //tapılmış kartların sayını artırır 
+                phoundCount = phoundCount + 1
+                if (phoundCount === cards.length / 2) {//eger butunn kartlar tapilibsa You Win! yazisini ekranda gorunur
+                    document.getElementById('you-win').style.display = 'block'
+                }
+            } else {//eger data idleri eynidirse onlara "data-pairid" statusu verilir
+                let prevCards = document.querySelectorAll(`[data-pairid='${persistedPairData}']`)
+                prevCards.forEach(function (item) {
+                    //hər bir acılan karta "card-open" statusu verilir ancaq o kartlardan sonra kilik etdiyimiz kart ilə idləri bir deilsə həmin kartda "card-open" statusu yox edilir 
+                    item.classList.remove('card-open');
+                })
+                persistedPairData = pairId;
+            }
         }
     }
-}
+})
+
